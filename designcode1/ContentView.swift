7334603e50a66fbd86a8580a2245c0bd003060b7
William Tomas <william.tomas@mobiapps.fr>
2020-07-10 13:19:32 +0200

    timing curve and delay animation

--- designcode1/ContentView.swift
+++ designcode1/ContentView.swift
@@ -12,11 +12,13 @@ struct ContentView: View {
     
     @State var show = false
     @State var viewState = CGSize.zero
+    @State var showCard = false
     
     var body: some View {
         ZStack {
             TitleView()
                 .blur(radius: show ? 20 : 0)
+                .opacity(showCard ? 0.4 : 1)
                 .animation(.default)
             
             BackCardView()
@@ -25,8 +27,10 @@ struct ContentView: View {
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -190 : 0)
                 .scaleEffect(0.9)
                 .rotationEffect(.degrees(show ? 0 : 10))
+                .rotationEffect(.degrees(showCard ? -10 : 0))
                 .rotation3DEffect(Angle(degrees: 10), axis: (x: 10, y: 0, z:0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5))
@@ -37,33 +41,37 @@ struct ContentView: View {
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -140 : 0)
                 .scaleEffect(0.95)
                 .rotationEffect(.degrees(show ? 0: 5))
+                .rotationEffect(.degrees(showCard ? -5 : 0))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10, y: 0, z:0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3))
             
             CardView()
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -100 : 0)
                 .blendMode(.hardLight)
                 .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
-                    self.show.toggle()
+                    self.showCard.toggle()
                 }
                 .gesture(
                     DragGesture().onChanged { value in
                         self.viewState = value.translation
-                        
+                        self.show = true
                     }
                     .onEnded { value in
                         self.viewState = .zero
-                        
+                        self.show = false
                     }
                 )
             
             BottomCardView()
+                .offset(x: 0, y: showCard ? 360 : 1000)
                 .blur(radius: show ? 20 : 0)
-                .animation(.default)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
             
         }
     }
@@ -149,6 +157,6 @@ struct BottomCardView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 600)
+        
     }
 }
