3b3b88e860d0aecdba0047cc5c71b2b4e72a947d
William Tomas <william.tomas.85@gmail.com>
2020-07-14 21:49:03 +0200

    color literal

--- designcode1/MenuView.swift
+++ designcode1/MenuView.swift
@@ -14,16 +14,38 @@ struct MenuView: View {
             Spacer()
             
             VStack(spacing: 16) {
+                Text("William - 28% completion")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 3)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 3, alignment: .leading)
+                    .background(Color.black.opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
