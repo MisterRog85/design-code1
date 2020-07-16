aa07bbe0a1075e1f54e93574f353152767047299
William Tomas <william.tomas@mobiapps.fr>
2020-07-16 13:26:46 +0200

    binding and screen size

---
+++ designcode1/HomeView.swift
@@ -0,0 +1,35 @@
+//
+//  HomeView.swift
+//  designcode1
+//
+//  Created by William Tomas on 16/07/2020.
+//  Copyright © 2020 William Tomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
