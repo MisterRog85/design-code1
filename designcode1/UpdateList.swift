a325df4ba53ec027a0c1f1cbee3c972cdb910b10
William Tomas <william.tomas.85@gmail.com>
2020-07-22 21:29:32 +0200

    navigation view and list

---
+++ designcode1/UpdateList.swift
@@ -0,0 +1,68 @@
+//
+//  UpdateList.swift
+//  designcode1
+//
+//  Created by William Tomas on 22/07/2020.
+//  Copyright © 2020 William Tomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        NavigationView {
+            List(updateData) { update in
+                NavigationLink(destination: Text(update.text)) {
+                    HStack {
+                        Image(update.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .background(Color.blue.opacity(0.2))
+                            .cornerRadius(20)
+                            .padding(.trailing, 4)
+                        
+                        VStack(alignment: .leading, spacing: 8.0) {
+                            Text(update.title)
+                                .font(.system(size: 20, weight: .bold))
+                            
+                            Text("Bonjour")
+                                .lineLimit(2)
+                                .font(.subheadline)
+                                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                            
+                            Text(update.date)
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+        .navigationBarTitle(Text("Updates"))
+        }
+    }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateList()
+    }
+}
+
+
+struct Update: Identifiable {
+    var id = UUID()
+    var image: String
+    var title: String
+    var text: String
+    var date: String
+}
+
+let updateData = [
+    Update(image: "Card1", title: "SwiftUI", text: "Text1", date: "19 Mai"),
+    Update(image: "Card1", title: "SwiftUI", text: "Text2", date: "19 Mai"),
+    Update(image: "Card1", title: "SwiftUI", text: "Text3", date: "19 Mai"),
+    Update(image: "Card1", title: "SwiftUI", text: "Text4", date: "19 Mai")
+]
