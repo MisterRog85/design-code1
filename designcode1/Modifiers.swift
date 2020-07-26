ddd4e24da9ce8f0275be7e678f970fc27ab05494
William Tomas <william.tomas.85@gmail.com>
2020-07-26 21:26:32 +0200

    custom fonts and modifiers

---
+++ designcode1/Modifiers.swift
@@ -0,0 +1,35 @@
+//
+//  Modifiers.swift
+//  designcode1
+//
+//  Created by William Tomas on 26/07/2020.
+//  Copyright © 2020 William Tomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShadowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
+        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+    }
+}
+
+struct FontModifier: ViewModifier {
+    var style: Font.TextStyle = .body
+    
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(style, design: .rounded))
+    }
+}
+
+struct CustomFontModifier: ViewModifier {
+    var size: CGFloat = 28
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Rubik-Bold", size: size))
+    }
+}
